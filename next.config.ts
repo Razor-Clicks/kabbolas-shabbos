daa8730a025990eba095760b196bb38e9fe367eb
Aaron Parnes <xpandopp@users.noreply.github.com>
2026-09-01 15:21:19 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ['@prisma/client'],
+};
+
+export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {

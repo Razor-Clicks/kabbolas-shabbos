e202790ebfa3a5ad469fa70ef84478e19e2b3682
Aaron Parnes <xpandopp@users.noreply.github.com>
2026-09-01 15:26:51 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;

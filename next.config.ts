d27c0017ba76aa08cb4e28e9fb58894d046ff98f
Aaron Parnes <xpandopp@users.noreply.github.com>
2026-09-01 17:10:52 -0400

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ['@prisma/client'],
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
